f6c1c1a3b930475bd5f876c581bdb5448109520f
vanguard6923 <lethanhlong6923@gmail.com>
2024-10-28 12:51:47 +0700

    landaydautien

---
+++ ViewComponents/MenuTopViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Harmic.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Harmic.ViewComponents
+{
+    public class MenuTopViewComponent : ViewComponent
+    {
+        private readonly HarmicContext _context;
+
+        public MenuTopViewComponent(HarmicContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var items = _context.TbMenus.Where(m => (bool)m.IsActive).OrderBy(m => m.Position).ToList();
+            return await Task.FromResult<IViewComponentResult>(View(items));
+        }
+    }
+}
---
+++ ViewComponents/ProductViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Harmic.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Harmic.ViewComponents
+{
+	public class ProductViewComponent : ViewComponent
+	{
+		private readonly HarmicContext _context;
+
+		public ProductViewComponent(HarmicContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var items = _context.TbProducts.Where(m => (bool)m.IsActive).Where(m => m.IsNew);
+			return await Task.FromResult<IViewComponentResult>(View(items.OrderByDescending(m => m.ProductId).ToList()));
+		}
+	}
+}
